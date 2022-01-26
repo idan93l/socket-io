459bd78d1bfc45c52ab04b94c57ba9543088a135
Idan Lev <idan.93lev@gmail.com>
2022-01-26 20:26:14 +0200

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,68 @@
+import { useState, useEffect } from "react";
+import { io } from "socket.io-client";
+import audioFile from "./soundTest1.mp3";
+
+const socket = io.connect("http://localhost:3001");
+
+const audio = new Audio();
+
+function App() {
+  const [role, setRole] = useState("");
+  const [playing, setPlaying] = useState("");
+
+  useEffect(() => {
+    const recieveMessage = (m) => {
+      console.log(m);
+      if (role === "ying") {
+        audio.src = m.path;
+        audio.play();
+      }
+      setPlaying(m.name);
+    };
+
+    const stopAudio = () => {
+      setPlaying('');
+    }
+
+    socket.on("play", recieveMessage);
+    socket.on('stop', stopAudio);
+
+    return () => {
+      socket.off("play", { name: "Test sound 1", path: audioFile });
+    };
+  }, [role]);
+
+  useEffect(() => {
+    const handleAudioStop = () => {
+      socket.emit('stop');
+    }
+    audio.addEventListener('pause', handleAudioStop)
+    return () => {
+      audio.removeEventListener('pause', handleAudioStop);
+    }
+  }, [])
+
+  const handlePlaySound = () => {
+    socket.emit("play", { name: "Test sound 1", path: audioFile });
+  };
+
+  return (
+    <div className="App">
+      <h1>Soundbutt</h1>
+      <div>
+        <h4>Role</h4>
+        <button onClick={() => setRole("yang")}>Yang</button>
+        <button onClick={() => setRole("ying")}>Ying</button>
+      </div>
+      <div>
+        <h4>Choose sound</h4>
+        <button onClick={handlePlaySound}>Play Sound!</button>
+      </div>
+      <div>
+        <h4>Playing {playing}</h4>
+      </div>
+    </div>
+  );
+}
+
+export default App;
